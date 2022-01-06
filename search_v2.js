46f42e92e4770d60926f538654991e74a05d8ba3
Thomas van der Pol <thomasvanderpol@gmail.com>
2022-01-06 16:17:52 +1000

    Improve target rendering

--- search_v2.js
+++ search_v2.js
@@ -1,19 +1,22 @@
 /** @param {NS} ns **/
 
 import NetworkScanner from "network_scanner.js"
+import NF from 'util_number_formatter.js'
 import GlobalMessenger from "global_messenger.js"
 
 export async function main(ns) {
     let messenger = new GlobalMessenger(ns, "SCNRDRKLY")
+    let nf = new NF(ns)
     let networkScanner = new NetworkScanner(ns, messenger)
+    networkScanner.mapServers()
 
-    if (ns.args.length < 2) {
-        ns.tprint("Please supply a command and a hostname to act on")
-    }
     let command = ns.args[0]
-    let targetHostname = ns.args[1]
 
     if ("search" == command) {
+        if (ns.args.length < 2) {
+            ns.tprint("Please supply a command and a hostname to act on")
+        }
+        let targetHostname = ns.args[1]
         let nodePath = networkScanner.findPath(targetHostname)
         let readablePath = "/" + nodePath.join("/")
         let connectablePath = "connect " + nodePath.slice(1, nodePath.length).join("; connect ") + ";"
@@ -27,6 +30,10 @@ export async function main(ns) {
         `)
         }
     } else if ("backdoor" == command) {
+        if (ns.args.length < 2) {
+            ns.tprint("Please supply a command and a hostname to act on")
+        }
+        let targetHostname = ns.args[1]
         let nodePath = networkScanner.findPath(targetHostname)
         if (typeof (nodePath) === String) {
             ns.tprint(nodePath)
@@ -34,6 +41,27 @@ export async function main(ns) {
             let connectablePath = "connect " + nodePath.slice(1, nodePath.length).join("; connect ") + ";"
             ns.tprint(connectablePath + " backdoor;")
         }
+    } else if ("targets" == command) {
+        let targets = networkScanner.currentTargetList(5)
+        let msg = "Current list of targets in priority order:\n"
+        for (const target of targets) {
+            msg += `
+                ================================
+                = hostname:    ${target.hostname}
+                ================================
+                =
+                = path:          /${networkScanner.findPath(target.hostname).join("/")}
+                = cash:          ${nf.money(target.moneyAvailable)} / ${nf.money(target.moneyMax)} (${(target.moneyAvailable / target.moneyMax * 100).toFixed(2)}%)
+                = hacking req:   ${target.requiredHackingSkill}
+                = diff:          ${target.hackDifficulty.toFixed(2)} / ${target.minDifficulty}
+                = growth:        ${ns.getServerGrowth(target.hostname)}
+                = hack time:     ${ns.tFormat(ns.getHackTime(target.hostname))}
+                = grow time:     ${ns.tFormat(ns.getGrowTime(target.hostname))}
+                = weaken time:   ${ns.tFormat(ns.getWeakenTime(target.hostname))}
+
+            `
+        }
+        ns.tprint(msg.split("\n").map(l => l.trim()).join("\n"))
     }
 }
 
